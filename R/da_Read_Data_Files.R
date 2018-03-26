bb0610fafa8a45b1d45368f77e8ce496980e0b1c
David <dalonso@ceab.csic.es>
2018-03-26 19:49:36 +0200

    Correcting errors in Examples of function da_Read_Data_Files

--- R/da_Read_Data_Files.R
+++ R/da_Read_Data_Files.R
@@ -1,11 +1,11 @@
 #' Reading a number of files from a directory into a list of data frames
 #'
-#' \code{da_Read_Data_Files} reads files from DATA_DIR into a list of 
-#' data frames. The full path of the directory should be specificied in 
-#' the DATA_DIR argument. For instance, if DATA_DIR = "[YOUR_DATA]", 
-#' then the function will read S files from "${HOME}/[YOUR_DATA]/" directory. Files 
-#' names are given in the \code{Names} character array of length S. The data frames 
-#' created are ready to feed the other package functions. 
+#' \code{da_Read_Data_Files} reads files from DATA_DIR into a list of
+#' data frames. The full path of the directory should be specificied in
+#' the DATA_DIR argument. For instance, if DATA_DIR = "[YOUR_DATA]",
+#' then the function will read S files from "${HOME}/[YOUR_DATA]/" directory. Files
+#' names are given in the \code{Names} character array of length S. The data frames
+#' created are ready to feed the other package functions.
 #'
 ## Input:
 #' @param DATA_DIR data directory to read from
@@ -15,6 +15,7 @@
 #' @keywords read.table, da_MacKenzie_mle.R
 #' @export
 #' @examples
+#'\dontrun{
 #  (1) Importing data to check da_upgma_AIC_cedp:
 #' HOME      <- Sys.getenv("HOME")
 #' DATA_DIR  <- "/PROJECT_ISLAND_BIOGEOGRAPHY/DATA_ILLES_LACADIVES/"
@@ -24,12 +25,12 @@
 #' Data <- da_Read_Data_Files(DATA_DIR, Names, S, H=TRUE)
 #
 #  (2) Importing data to check da_MacKenzie_mle.R
-#' Names = c("MacKenzie_Data_Matrix_AGT_156Sp_Header_TRUE.dat", 
-#' "MacKenzie_Data_Matrix_KAD_156Sp_Header_TRUE.dat", 
+#' Names = c("MacKenzie_Data_Matrix_AGT_156Sp_Header_TRUE.dat",
+#' "MacKenzie_Data_Matrix_KAD_156Sp_Header_TRUE.dat",
 #' "MacKenzie_Data_Matrix_KVT_156Sp_Header_TRUE.dat");
-#' S <- 3 
+#' S <- 3
 #' Data <- da_Read_Data_Files(DATA_DIR, Names, S, H=TRUE)
-
+#'}
 da_Read_Data_Files <- function( DATA_DIR, Names, S, H=TRUE )
 {
   # We can create a list of data frames from each of the files
@@ -40,13 +41,8 @@ da_Read_Data_Files <- function( DATA_DIR, Names, S, H=TRUE )
     File <- paste(DATA_DIR, Names[i], sep="")
     DATALIST[[i]] <- read.table(File, header=H)
   }
-  
-  # This is a data list of data frames containing all data, 
-  # one member in the list per file read. 
+
+  # This is a data list of data frames
+  # (one list member per file to be read).
   DATALIST;
 }
-
-
-
-
-
--- src/upgma_clustering.c
+++ src/upgma_clustering.c
@@ -2,25 +2,25 @@
 #include <da_IBT_Functions.h>
 
 // #defined VERBOSE
-double Average_Node_Distance( node * N_1, node * N_2, 
+double Average_Node_Distance( node * N_1, node * N_2,
 			      double ** D, int S )
 {
   int i,j;
-  int sp_i, sp_j; 
+  int sp_i, sp_j;
 
   double Ave_D;
   Ave_D = 0.0;
   for(i=0; i<N_1->No_of_SPECIES; i++)
     for(j=0; j<N_2->No_of_SPECIES; j++){
-      sp_i = N_1->Species[i]; 
+      sp_i = N_1->Species[i];
       sp_j = N_2->Species[j];
 
       if(sp_i >= S || sp_j >= S) error(0,0,"Program aborted");
-      
+
       if ( sp_i > sp_j ) Ave_D += D[sp_i][ sp_j];
       else               Ave_D += D[sp_j][ sp_i];
     }
-  
+
   Ave_D = Ave_D / (double)(N_1->No_of_SPECIES * N_2->No_of_SPECIES);
 
   return(Ave_D);
@@ -29,16 +29,16 @@ double Average_Node_Distance( node * N_1, node * N_2,
 void Print_Triangular_Matrix( double ** distance, int N )
 {
   int i,j;
-  
+
   for(i=1; i<N; i++) {
-    for(j=0; j<i; j++) 
+    for(j=0; j<i; j++)
        Rprintf(" [ d(%d, %d) = %g ]", i,j, distance[i][j] );
     Rprintf("\n");
   }
 }
 
-void upgma_clustering(double ** D, int No_of_SPECIES,  
-		      double ** distance, int * n, 
+void upgma_clustering(double ** D, int No_of_SPECIES,
+		      double ** distance, int * n,
 		      node ** T, int * No_of_NODES, int * Node_List )
 {
   /* Input:
@@ -46,13 +46,13 @@ void upgma_clustering(double ** D, int No_of_SPECIES,
      through the whole algorithm.
      . S: Number of rows and columns of the initial distance matrix.
      . distance[][]: Distances between clusters as new clusters arise
-     . (*n) Number of rows/columns of distance[][],  which is reduced by 
+     . (*n) Number of rows/columns of distance[][],  which is reduced by
      one at each recurrent call
-     . (* No_of_NODES): total number of nodes of the tree, which is 
+     . (* No_of_NODES): total number of nodes of the tree, which is
      increased by a new inner node at each recurrent call.
-     . Node_List[], list of nodes to compare in the "shrinking" input 
-     distance[][] matrix, from first to last column/row. 
-     
+     . Node_List[], list of nodes to compare in the "shrinking" input
+     distance[][] matrix, from first to last column/row.
+
      Output:
      T: the tree countaning the cluster analysis. The root node is always
      node * root = T[(*No_of_NODES)-1]
@@ -64,23 +64,23 @@ void upgma_clustering(double ** D, int No_of_SPECIES,
   if( N > 1) {
     double ** temp  = (double **)calloc( N, sizeof(double *) );
     for(i=1; i<N; i++) temp[i] = (double *)calloc( i, sizeof(double) );
-    for(i=1; i<N; i++) 
-      for(j=0; j<i; j++) temp[i][j] = distance[i][j]; 
+    for(i=1; i<N; i++)
+      for(j=0; j<i; j++) temp[i][j] = distance[i][j];
 
     // Print_Triangular_Matrix( temp, N );
     // getchar();
 
     int * Index_List = (int *)calloc( N, sizeof(int) );
     int * List = (int *)calloc( N, sizeof(int) );
-    for(i=0; i<N; i++) List[i] = Node_List[i]; 
+    for(i=0; i<N; i++) List[i] = Node_List[i];
 
     i_MIN = 1;
     j_MIN = 0;
-    x     = temp[i_MIN][j_MIN]; 
-    for(i=1; i<N; i++) 
-      for(j=0; j<i; j++) { 
-	if( temp[i][j] < x ) { 
-	  i_MIN = i; 
+    x     = temp[i_MIN][j_MIN];
+    for(i=1; i<N; i++)
+      for(j=0; j<i; j++) {
+	if( temp[i][j] < x ) {
+	  i_MIN = i;
 	  j_MIN = j;
 	}
 	x = MIN( temp[i][j], x );
@@ -93,18 +93,18 @@ void upgma_clustering(double ** D, int No_of_SPECIES,
     int S = 0;
     for(i=0; i<T[List[i_MIN]]->No_of_SPECIES; i++) T[ * No_of_NODES ]->Species[S++] = T[List[i_MIN]]->Species[i];
     for(i=0; i<T[List[j_MIN]]->No_of_SPECIES; i++) T[ * No_of_NODES ]->Species[S++] = T[List[j_MIN]]->Species[i];
-    T[ * No_of_NODES ]->No_of_SPECIES =  S; 
-    if ( S != (T[List[i_MIN]]->No_of_SPECIES + T[List[j_MIN]]->No_of_SPECIES) ) error(0,0,"Program aborted"); 
-      
+    T[ * No_of_NODES ]->No_of_SPECIES =  S;
+    if ( S != (T[List[i_MIN]]->No_of_SPECIES + T[List[j_MIN]]->No_of_SPECIES) ) error(0,0,"Program aborted");
+
     Node_List[0] = (* No_of_NODES);
-    
+
     k = 1; m = 0;
     for(i=0; i<N; i++) {
       if (i != i_MIN && i != j_MIN) {
-	
-	distance[k][0]  = Average_Node_Distance( T[ * No_of_NODES ], T[ List[ i ] ], 
+
+	distance[k][0]  = Average_Node_Distance( T[ * No_of_NODES ], T[ List[ i ] ],
 						 D, No_of_SPECIES );
-       
+
 	Node_List[k]    =  List[i];
 	Index_List[m++] =  i;
 	k++;
@@ -113,13 +113,13 @@ void upgma_clustering(double ** D, int No_of_SPECIES,
 
     if (m != N-2) error("Program aborted");
 
-    k = 1; 
+    k = 1;
     for(i=0; i<m; i++) {
       l = 1;
       for(j=i+1; j<m; j++) {
 	if ( Index_List[i] > Index_List[j] ) x = temp[ Index_List[i] ][ Index_List[j] ];
 	else                                 x = temp[ Index_List[j] ][ Index_List[i] ];
-	
+
 	distance[k+l][k] = x;
 	l++;
       }
@@ -129,13 +129,13 @@ void upgma_clustering(double ** D, int No_of_SPECIES,
     * n = N-1;
     (* No_of_NODES) = (* No_of_NODES) + 1;
 
-#if defined VERBOSE    
+#if defined VERBOSE
     Print_Triangular_Matrix( distance, * n );
     Rprintf("--------------------------------\n\n");
 #endif
-    upgma_clustering( D, No_of_SPECIES, 
+    upgma_clustering( D, No_of_SPECIES,
 		      distance, n, T, No_of_NODES, Node_List );
-        
+
     for(i=1; i<N; i++) free(temp[i]);
     free(temp);
     free(List); free(Index_List);
@@ -237,5 +237,8 @@ node * search(node ** tree, double val)
     {
         return *tree;
     }
-}
 
+   // The function will return the NULL pointer when everything else fails
+	 // (solving the warning: "warning: control reaches end of non-void function [-Wreturn-type]")
+		return NULL;
+}
