be36289ba5f6488c28d561152d6685c0ee76b993
vankampen92 <dalonso@ceab.csic.es>
2019-04-09 18:21:36 +0200

    Modifying init.c and removing old file R_SHLIB___mle_NLLikelihood_Minimization.c

--- src/init.c
+++ src/init.c
@@ -10,14 +10,14 @@ extern void MODEL_SELECTION_UPGMA_MacKENZIE_R_FUNCTION(void *, void *, void *, v
 extern void MODEL_SELECTION_UPGMA_R_FUNCTION(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void R_SHLIB___mle_MacKenzie_NLLikelihood_Minimization(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void R_SHLIB___mle_MacKenzie_NLL_Uneven_Minimization(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-extern void R_SHLIB___mle_NLLikelihood_Minimization(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void R_SHLIB___mle_NLL_Uneven_Minimization(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_CMethodDef CEntries[] = {
     {"MODEL_SELECTION_UPGMA_MacKENZIE_R_FUNCTION",        (DL_FUNC) &MODEL_SELECTION_UPGMA_MacKENZIE_R_FUNCTION,        24},
     {"MODEL_SELECTION_UPGMA_R_FUNCTION",                  (DL_FUNC) &MODEL_SELECTION_UPGMA_R_FUNCTION,                  20},
     {"R_SHLIB___mle_MacKenzie_NLLikelihood_Minimization", (DL_FUNC) &R_SHLIB___mle_MacKenzie_NLLikelihood_Minimization, 23},
     {"R_SHLIB___mle_MacKenzie_NLL_Uneven_Minimization",   (DL_FUNC) &R_SHLIB___mle_MacKenzie_NLL_Uneven_Minimization,   24},
-    {"R_SHLIB___mle_NLLikelihood_Minimization",           (DL_FUNC) &R_SHLIB___mle_NLLikelihood_Minimization,           20},
+    {"R_SHLIB___mle_NLL_Uneven_Minimization",             (DL_FUNC) &R_SHLIB___mle_NLL_Uneven_Minimization,             20},
     {NULL, NULL, 0}
 };
 
